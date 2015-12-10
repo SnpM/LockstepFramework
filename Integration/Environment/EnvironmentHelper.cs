ed761898683b606ba750eec5865a746f2af01c19
SnpM <jpthek9@gmail.com>
2015-12-10 12:15:41 -0700

    Add EnvironmentHelper and tweak scene

---
+++ Integration/Environment/EnvironmentHelper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Lockstep
+{
+    public class EnvironmentHelper : BehaviourHelper
+    {
+        public override InputCode ListenInput
+        {
+            get
+            {
+                return InputCode.None;
+            }
+        }
+
+        public EnvironmentSaver Saver { get; private set; }
+
+        public EnvironmentHelper(EnvironmentSaver saver)
+        {
+            this.Saver = saver;
+        }
+
+        protected override void OnInitialize()
+        {
+            if (Saver.EnvironmentBodies == null)
+            {
+                Debug.Log("No EnvironmentBodies found in Saver - Ignoring.");
+            } else
+            {
+                foreach (EnvironmentBodyInfo info in Saver.EnvironmentBodies)
+                {
+                    info.Body.Initialize(info.Position, info.Rotation);
+                }
+            }
+        }
+    }
+}
