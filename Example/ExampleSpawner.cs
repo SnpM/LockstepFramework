0a1b346b171e43b3271ae5a726f84b8d9a5e8708
SnpM <jpthek9@gmail.com>
2016-01-04 20:13:35 -0700

    Merge

---
+++ Example/ExampleSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Lockstep.Example
+{
+	public class ExampleSpawner : BehaviourHelper
+	{
+		public override ushort ListenInput {
+			get {
+				return InputCodeManager.GetCodeID("Spawn");
+			}
+		}
+		/*
+		 *			Command com = new Command (InputCodeManager.GetCodeID ("Spawn"));
+            com.ControllerID = cont.ControllerID;
+            com.Position = position;
+            com.Target = (ushort)AgentController.GetAgentCodeIndex(agentCode);
+            com.Count = count;
+            return com; 
+		 */
+		protected override void OnExecute (Command com)
+		{
+			byte conID = com.ControllerID;
+            Vector2d pos = com.GetData<Vector2d>(0);
+            Vector2d rot = com.GetData<Vector2d>(1);
+            ushort target = (ushort)com.GetData<DefaultData>(0).Value;
+            ushort count = (ushort)com.GetData<DefaultData>(1).Value;
+
+			AgentController ac = AgentController.InstanceManagers [conID];
+			string agentCode = AgentController.GetAgentCode (target);
+            Debug.Log(rot);
+			for (int i = 0; i < count; i++) {
+				ac.CreateAgent (agentCode, pos, rot);
+			}
+		}
+	}
+}
