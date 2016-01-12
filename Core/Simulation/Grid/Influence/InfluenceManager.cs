cd1c31082d8b3e8831d9aec3aac587cc779ff71a
GladFox <9006003@gmail.com>
2016-01-13 03:20:03 +0600

    Add ScanCone

--- Core/Simulation/Grid/Influence/InfluenceManager.cs
+++ Core/Simulation/Grid/Influence/InfluenceManager.cs
@@ -29,38 +29,49 @@ namespace Lockstep
 
         const int FoundScanBuffer = 5;
 
-        public static LSAgent Scan(int gridX, int gridY, int deltaCount,
-                                   LSAgent sourceAgent, AllegianceType targetAllegiance)
-        {
-            long sourceX = sourceAgent.Body._position.x;
-            long sourceY = sourceAgent.Body._position.y;
-            LSAgent closestAgent = null;
-            long closestDistance = 0;
-            int foundBuffer = FoundScanBuffer;
-            foreach (LSAgent agent in ScanAll (gridX, gridY, deltaCount, sourceAgent, targetAllegiance))
-            {
-                if (FoundScanBuffer == 0)
-                    break;
-                if (closestAgent != null)
-                {
-                    long tempDistance = agent.Body._position.FastDistance(sourceX, sourceY);
-                    if (tempDistance < closestDistance)
-                    {
-                        closestAgent = agent;
-                        closestDistance = tempDistance;
-                        foundBuffer = FoundScanBuffer;
-                    }
-                    else {
-                        foundBuffer--;
-                    }
-                } else
-                {
-                    closestAgent = agent;
-                    closestDistance = agent.Body._position.FastDistance(sourceX, sourceY);
-                }
-            }
-            return closestAgent;
-        }
+		public static LSAgent FindClosestAgent(LSAgent sourceAgent, IEnumerable<LSAgent> agents)
+		{
+			long sourceX = sourceAgent.Body._position.x;
+			long sourceY = sourceAgent.Body._position.y;
+			LSAgent closestAgent = null;
+			long closestDistance = 0;
+			int foundBuffer = FoundScanBuffer;
+			foreach (LSAgent agent in agents)
+			{
+				if (FoundScanBuffer == 0)
+					break;
+				if (closestAgent != null)
+				{
+					long tempDistance = agent.Body._position.FastDistance(sourceX, sourceY);
+					if (tempDistance < closestDistance)
+					{
+						closestAgent = agent;
+						closestDistance = tempDistance;
+						foundBuffer = FoundScanBuffer;
+					}
+					else {
+						foundBuffer--;
+					}
+				} else
+				{
+					closestAgent = agent;
+					closestDistance = agent.Body._position.FastDistance(sourceX, sourceY);
+				}
+			}
+			return closestAgent;
+		}
+
+		public static LSAgent Scan(int gridX, int gridY, int deltaCount,
+			LSAgent sourceAgent, AllegianceType targetAllegiance)
+		{
+			return FindClosestAgent(sourceAgent, ScanAll (gridX, gridY, deltaCount, sourceAgent, targetAllegiance));
+		}
+
+		public static LSAgent ScanCone(int gridX, int gridY, long radius, long angle,
+			LSAgent sourceAgent, AllegianceType targetAllegiance)
+		{
+			return FindClosestAgent(sourceAgent, ScanAllCone (gridX, gridY, radius, angle, sourceAgent, targetAllegiance));
+		}
 
         public static IEnumerable<LSAgent> ScanAll(int gridX, int gridY, int deltaCount,
                                                    LSAgent sourceAgent,
@@ -94,6 +105,58 @@ namespace Lockstep
                 }
             }
         }
+
+		public static IEnumerable<LSAgent> ScanAllCone(int gridX, int gridY, long radius, long angle,
+			LSAgent sourceAgent,
+			AllegianceType targetAllegiance)
+		{
+			Vector2d center = sourceAgent.Body._position;
+			Vector2d rotation = sourceAgent.Body._rotation;
+
+			int deltaCount = InfluenceManager.GenerateDeltaCount (radius);
+			long num = radius * radius;
+			long num2 = angle * angle >> 16;
+
+			long sourceX = sourceAgent.Body._position.x;
+			long sourceY = sourceAgent.Body._position.y;
+			for (int i = 0; i < deltaCount; i++)
+			{
+				ScanNode tempNode = GridManager.GetScanNode(
+					gridX + DeltaCache.CacheX [i],
+					gridY + DeltaCache.CacheY [i]);
+
+				if (tempNode.IsNotNull())
+				{
+					foreach (FastBucket<LSInfluencer> tempBucket in tempNode.BucketsWithAllegiance(sourceAgent, targetAllegiance))
+					{
+						BitArray arrayAllocation = tempBucket.arrayAllocation;
+						for (int j = 0; j < tempBucket.PeakCount; j++)
+						{
+							if (arrayAllocation.Get(j))
+							{
+								LSAgent tempAgent = tempBucket [j].Agent;
+
+								Vector2d agentPos = tempAgent.Body._position;
+								Vector2d difference = agentPos - center;
+
+								long num3 = difference.FastMagnitude ();
+								if (num3 <= num && difference.Dot (rotation.x, rotation.y) > 0L)
+								{
+									num3 >>= 16;
+									long num4 = rotation.Cross (difference.x, difference.y);
+									num4 *= num4;
+									num4 >>= 16;
+									if (num4 < num2 * num3 >> 16)
+									{
+										yield return tempAgent;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 
         public static LSAgent Source;
         public static PlatformType TargetPlatform;
