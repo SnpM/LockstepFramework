d4af69902dd20e52a4616f21f18d0033cd98858d
SnpM <jpthek9@gmail.com>
2016-01-13 12:22:00 -0700

    Add PathObject for dynamic loading

---
+++ Core/Utility/PathObject/Core/PathObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public class PathObject {
+    [SerializeField]
+    protected string _path;
+    public string Path {get {return _path;}}
+
+    bool Setted = false;
+    private UnityEngine.Object _object;
+    public UnityEngine.Object Object {
+        get {
+            return Setted ? _object : (_object = PathObjectFactory.Load (this));
+        }
+    }
+}
---
+++ Core/Utility/PathObject/Core/PathObjectAttribute.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+public class PathObjectAttribute : UnityEngine.PropertyAttribute {
+    public Type ObjectType {get; private set;}
+
+    public PathObjectAttribute (Type objectType) {
+        this.ObjectType = objectType;
+        if (objectType.IsSubclassOf(typeof (UnityEngine.Component)) == false) {
+            throw new ArgumentException(string.Format("Type '{0}' is not a UnityEngine.Component.", objectType));
+        }
+    }
+}
