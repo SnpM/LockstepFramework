7729a2a9a87a3f4872f36c120f4d7b7accc4e19a
SnpM <jpthek9@gmail.com>
2016-03-02 10:20:28 -0700

    Refactor Blocker and add DynamicBlocker

--- Core/Simulation/Grid/Utility/Tools/Blocker/Blocker.cs
+++ Core/Simulation/Grid/Utility/Tools/Blocker/Blocker.cs
@@ -35,5 +35,7 @@ public class Blocker : EnvironmentObject
             }
         }
     }
+
+
         
 }
---
+++ Core/Simulation/Grid/Utility/Tools/Blocker/DynamicBlocker.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Lockstep
+{
+    public class DynamicBlocker : Ability
+    {
+        static readonly FastList<Vector2d> bufferCoordinates = new FastList<Vector2d>();
+
+        LSBody CachedBody;
+
+        protected override void OnInitialize()
+        {
+            CachedBody = Agent.Body;
+            UpdateCoordinates ();
+        }
+
+        void RemoveLastCoordinates () {
+            for (int i = 0; i < LastCoordinates.Count; i++) {
+                GridNode node = LastCoordinates[i];
+                node.RemoveObstacle();
+            }
+            LastCoordinates.FastClear();
+        }
+        void UpdateCoordinates () {
+            const long gridSpacing = FixedMath.One;
+            bufferCoordinates.FastClear();
+            CachedBody.GetCoveredSnappedPositions(gridSpacing, bufferCoordinates);
+            foreach (Vector2d vec in bufferCoordinates)
+            {
+                GridNode node = GridManager.GetNode(vec.x, vec.y);
+
+                if (node == null)
+                    continue;
+
+                node.AddObstacle();
+                LastCoordinates.Add(node);
+            }
+        }
+
+
+        FastList<GridNode> LastCoordinates = new FastList<GridNode>();
+        protected override void OnLateSimulate()
+        {
+            if (this.CachedBody.PositionChangedBuffer)
+            {
+                RemoveLastCoordinates ();
+                UpdateCoordinates ();
+
+
+            }
+        }
+    }
+}
