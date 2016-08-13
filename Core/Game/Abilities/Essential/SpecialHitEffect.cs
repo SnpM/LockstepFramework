db61e67ea94fff6d19666292f29b5e318359861d
SnpM <jpthek9@gmail.com>
2016-08-12 20:49:25 -0600

    Sync

---
+++ Core/Game/Abilities/Essential/SpecialHitEffect.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Lockstep;
+namespace Lockstep
+{
+	public class SpecialHitEffect : DurationAbility
+	{
+		protected Scan cachedScan { get; private set;}
+
+		protected override void OnSetup()
+		{
+			cachedScan = Agent.GetAbility<Scan>();
+		}
+		protected override void OnExecute(Command com)
+		{
+			base.OnExecute(com);
+		}
+
+		protected override void OnStartWorking()
+		{
+			cachedScan.ExtraOnHit += ApplyEffect;
+		}
+		protected virtual void ApplyEffect(LSAgent agent)
+		{
+			OnApplyEffect(agent);
+		}
+		protected virtual void OnApplyEffect (LSAgent agent)
+		{
+		}
+
+		protected override void OnStopWorking()
+		{
+			cachedScan.ExtraOnHit -= ApplyEffect;
+		}
+	}
+}
