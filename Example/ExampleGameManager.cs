ee38a0e62b57781100d984ab4944935c58000b9a
SnpM <jpthek9@gmail.com>
2016-08-29 19:44:45 -0600

    Improve replay test UI

--- Example/ExampleGameManager.cs
+++ Example/ExampleGameManager.cs
@@ -4,44 +4,34 @@ using System.Collections;
 using Lockstep.Data;
 using TypeReferences;
 using System;
-namespace Lockstep.Example {
-    public class ExampleGameManager : GameManager {
-        Replay LastSave = new Replay();
-		void OnGUI ()
-		{
-			GUI.matrix = Matrix4x4.TRS (new Vector3(0, 0, 0), Quaternion.identity, new Vector3 (2.5f, 2.5f, 1)); 
+
+namespace Lockstep.Example
+{
+    public class ExampleGameManager : GameManager
+    {
+        static Replay LastSave = new Replay();
+
+        void OnGUI()
+        {
+            GUI.matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), Quaternion.identity, new Vector3(2.5f, 2.5f, 1)); 
 
 
-			if (ReplayManager.IsPlayingBack) {
-				if (ReplayManager.CurrentReplay != null)
-				{
-					if (GUILayout.Button("Stop"))
-					{
-						ReplayManager.CurrentReplay = null;
-						ReplayManager.Stop();
-						LSUtility.LoadLevel(SceneManager.GetActiveScene().name);
-					}
 
-					if (GUILayout.Button("Rewind"))
-					{
-						ReplayManager.Play(LastSave);
-						LSUtility.LoadLevel(SceneManager.GetActiveScene().name);
-					}
-				}
+            if (GUILayout.Button("Restart"))
+            {
+                ReplayManager.Stop();
+                LSUtility.LoadLevel(SceneManager.GetActiveScene().name);
+            }
 
-			} else {
-				if (GUILayout.Button ("Restart")) {
-					LSUtility.LoadLevel (SceneManager.GetActiveScene().name);
-				}
+            if (GUILayout.Button("Playback"))
+            {
+                LastSave = ReplayManager.SerializeCurrent();
+                LSUtility.LoadLevel(SceneManager.GetActiveScene().name);
+                ReplayManager.Play(LastSave);
 
-				if (GUILayout.Button ("Save")) {
-                    LastSave = ReplayManager.SerializeCurrent();
-					LSUtility.LoadLevel (SceneManager.GetActiveScene().name);
-                    ReplayManager.Play (LastSave);
-
-				}
-			}
-		}
+            }
+			
+        }
 
 
     }
