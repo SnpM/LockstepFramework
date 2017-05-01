80073ea6ca61566d0201f35e6c56c24d462734b8
jpthek9 <jpthek9@gmail.com>
2017-05-01 11:02:02 -0600

    Add commenting to the gathering process

--- Core/Game/Player/RTSInterfacingHelper.cs
+++ Core/Game/Player/RTSInterfacingHelper.cs
@@ -11,6 +11,9 @@ namespace Lockstep
 
         private static AbilityDataItem _currentInterfacer;
 
+        /// <summary>
+        /// The current ability to cast. Set this to a non-null value to automatically start the gathering process.
+        /// </summary>
         public static AbilityDataItem CurrentInterfacer
         {
             get { return _currentInterfacer; }
@@ -22,6 +25,11 @@ namespace Lockstep
                 }
                 _currentInterfacer = value;
             }
+        }
+        //Helper function that takes in a type rather than AbilityDataItem to cast an ability
+        public static void CastAbility<TAbility> () where TAbility : ActiveAbility
+        {
+            CurrentInterfacer = AbilityDataItem.FindInterfacer<TAbility>();
         }
 
         public static AbilityDataItem QuickPos;
@@ -66,36 +74,45 @@ namespace Lockstep
 
         protected override void OnVisualize()
         {
+            //Update the SelectionManager which handles box-selection.
             SelectionManager.Update();
-
-
+            //Update RTSInterfacing, a useful tool that automatically generates useful data for user-interfacing
             RTSInterfacing.Visualize();
 
             if (IsGathering)
             {
+                //We are currently gathering mouse information. The next click will trigger the command with the mouse position.
+                //I.e. Press "T" to use the 'Psionic Storm' ability. Then left click on a position to activate it there.
+
+                //Right click to cancel casting the abiility by setting IsGathering to false
                 if (Input.GetMouseButtonDown(1))
                 {
                     IsGathering = false;
                     return;
                 }
 
+                //If we left click to release the ability
+                //Or if the ability we're activating requires no mouse-based information (i.e. CurrentInterfacer.InformationGather)
+                //Trigger the ability
                 if (Input.GetMouseButtonDown(0) || CurrentInterfacer.InformationGather == InformationGatherType.None)
                 {
                     ProcessInterfacer(CurrentInterfacer);
                 }
             } else
             {
+                //We are not gathering information. Instead, allow quickcasted abilities with the mouse. I.e. Right click to move or attack.
                 if (Selector.MainSelectedAgent != null)
                 {
                     if (Input.GetMouseButtonDown(1))
                     {
-                        //LSAgent target;
                         if (RTSInterfacing.MousedAgent.IsNotNull() &&
-						    Selector.MainSelectedAgent.GetAbility<Scan>() != null)
+					        Selector.MainSelectedAgent.GetAbility<Scan>() != null)
                         {
+                            //If the selected agent has Scan (the ability behind attacking) and the mouse is over an agent, send a target command - right clicking on a unit
                             ProcessInterfacer((QuickTarget));
                         } else
                         {
+                            //If there is no agent under the mouse or the selected agent doesn't have Scan, send a Move command - right clicking on terrain
                             ProcessInterfacer((QuickPos));
                         }
                     }
